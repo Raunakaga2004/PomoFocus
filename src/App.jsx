4f488a6bc88744f9b2a5e0d3222523c99b443a56
Raunakaga2004 <raunakaga12@gmail.com>
2025-07-19 19:27:01 +0530

    setting menu added

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,24 @@ function App() {
 
   const [setting, setSetting] = useState(false);
 
+  // timer setting variables
+  const [pomoMin, setPomoMin] = useState(25);
+  const [pomoSec, setPomoSec] = useState(0);
+
+  const [shortMin, setShortMin] = useState(5);
+  const [shortSec, setShortSec] = useState(0);
+
+  const [longMin, setLongMin] = useState(15);
+  const [longSec, setLongSec] = useState(0);
+
+  const [focusSession, setFocusSession] = useState(4);
+
   const handleSettingsButton = ()=>{
     setSetting(true);
+  }
+
+  const handleCloseSetting = ()=>{
+    setSetting(false);
   }
 
   return (
@@ -18,15 +34,59 @@ function App() {
 
       {/* Settings */}
       {setting && <>
-        <div className='absolute bg-white min-h-[100px] min-w-[100px] z-3 rounded-lg'>
-          <div className='absolute top-1 right-1'>
+        <div className='absolute bg-white min-h-[300px] max-h-[300px] max-w-[300px] min-w-[300px] z-3 rounded-lg p-6'>
+          <div className='absolute top-1 right-1' onClick={handleCloseSetting}>
             <svg className="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6"/>
             </svg>
           </div>
 
+          {/* content of setting */}
+          <div className='text-center text-xs mb-[30px]'>
+            Set the time according to your desire and then close the menu
+          </div>
+          <div className='flex flex-col justify-center gap-5'>
+            <label className='flex justify-between'>
+              Pomodoro
+              <input type='number' defaultValue={pomoMin} className='w-[30px]  bg-slate-100 rounded-lg text-center outline-none border-b-2 border-r-2 border-slate-400' onChange={(e)=>{
+                if(e.target.value >= 0)
+                  setPomoMin(e.target.value)}}/>
+              :
+              <input type='number'defaultValue={pomoSec} className='w-[30px]  bg-slate-100 rounded-lg text-center outline-none border-b-2 border-r-2 border-slate-400' onChange={(e)=> {
+                if(e.target.value < 60 && e.target.value >= 0)
+                  setPomoSec(e.target.value)}}/>
+            </label>
+
+            <label className='flex justify-between'>
+              Short Rest
+              <input type='number' defaultValue={shortMin} className='w-[30px]  bg-slate-100 rounded-lg text-center outline-none border-b-2 border-r-2 border-slate-400' onChange={(e)=>{
+                if(e.target.value >= 0)
+                  setShortMin(e.target.value)}}/>
+              :
+              <input type='number'defaultValue={shortSec} className='w-[30px]  bg-slate-100 rounded-lg text-center outline-none border-b-2 border-r-2 border-slate-400' onChange={(e)=>{
+                if(e.target.value < 60 && e.target.value >= 0)
+                  setShortSec(e.target.value)}}/>
+            </label>
+
+            <label className='flex justify-between'>
+              Long Rest
+              <input type='number' defaultValue={longMin} className='w-[30px]  bg-slate-100 rounded-lg text-center outline-none border-b-2 border-r-2 border-slate-400' onChange={(e)=>{
+                if(e.target.value >= 0)
+              setLongMin(e.target.value)}}/>
+              :
+              <input type='number'defaultValue={longSec} className='w-[30px]  bg-slate-100 rounded-lg text-center outline-none border-b-2 border-r-2 border-slate-400' onChange={(e)=>{
+                if(e.target.value < 60 && e.target.value >= 0)
+                  setLongSec(e.target.value)}}/>
+            </label>
+
+            <label className='flex justify-between'>
+              Number of focus Session before Long break
+              <input type='number'defaultValue={focusSession} className='w-[30px]  bg-slate-100 rounded-lg text-center outline-none border-b-2 border-r-2 border-slate-400' onChange={(e)=>setFocusSession(e.target.value)}/>
+            </label>
+          </div>
+
         </div>
-        <div className='absolute bg-slate-400 min-h-[100px] min-w-[100px] z-2 rounded-lg translate-x-[3px] translate-y-[4px]'></div>
+        <div className='absolute bg-slate-400 min-h-[300px] min-w-[300px] z-2 rounded-lg translate-x-[3px] translate-y-[4px]'></div>
         <div className='h-screen w-screen absolute bg-black z-1 opacity-80 backdrop-blur-sm flex justify-center items-center'>
         </div>
       </>}
@@ -46,7 +106,7 @@ function App() {
         </svg>
       </div>
       
-      <Timer pomomin={25} pomosec={0} shortmin={5} shortsec={0} longmin={15} longsec={0} focusSession={4} setbgcolor={setbgcolor} bgcolor={bgcolor}/>
+      <Timer pomomin={pomoMin} pomosec={pomoSec} shortmin={shortMin} shortsec={shortSec} longmin={longMin} longsec={longSec} focusSession={focusSession} setbgcolor={setbgcolor} bgcolor={bgcolor}/>
     </div>
   )
 }
--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -9,21 +9,54 @@ let shortBreakCount = 0;
 
 export function Timer(props){
     const [timertype, setTimerType] = useState("Pomodoro");
+    
+    const [pomomin, setPomomin] = useState(props.pomomin);
+    const [pomosec, setPomosec] = useState(props.pomosec);
+    const [shortmin, setShortmin] = useState(props.shortmin);
+    const [shortsec, setShortsec] = useState(props.shortsec);
+    const [longmin, setLongmin] = useState(props.longmin);
+    const [longsec, setLongsec] = useState(props.longsec);
+    const [focusSession, setFocusSession] = useState(props.focusSession); 
 
     const curMin = ()=>{
-        if(timertype == "Pomodoro") return props.pomomin;
-        else if (timertype == "Short Break") return props.shortmin;
-        else return props.longmin;
+        if(timertype == "Pomodoro") return pomomin;
+        else if (timertype == "Short Break") return shortmin;
+        else return longmin;
     }
 
     const curSec = ()=>{
-        if(timertype == "Pomodoro") return props.pomosec;
-        else if (timertype == "Short Break") return props.shortsec;
-        else return props.longsec;
+        if(timertype == "Pomodoro") return pomosec;
+        else if (timertype == "Short Break") return shortsec;
+        else return longsec;
     }
+
+    useEffect(()=>{
+
+        console.log(props.pomomin);
+        setPomomin(props.pomomin);
+        setPomosec(props.pomosec);
+        setShortmin(props.shortmin);
+        setShortsec(props.shortsec);
+        setLongmin(props.longmin);
+        setLongsec(props.longsec);
+        setFocusSession(props.focusSession);
+    }, [props])
 
     const [Min, setMin] = useState(curMin);
     const [Sec, setSec] = useState(curSec);
+
+    useEffect(() => {
+        if(timertype === "Pomodoro"){
+            setMin(pomomin);
+            setSec(pomosec);
+        } else if(timertype === "Short Break"){
+            setMin(shortmin);
+            setSec(shortsec);
+        } else if(timertype === "Long Break"){
+            setMin(longmin);
+            setSec(longsec);
+        }
+    }, [pomomin, pomosec, shortmin, shortsec, longmin, longsec, timertype]);
 
     const [isTimerOn, setTimer] = useState(false);
 
@@ -51,7 +84,7 @@ export function Timer(props){
             else{
                 alarm.play();
                 if(timertype == "Pomodoro"){
-                    if(shortBreakCount == props.focusSession-1){
+                    if(shortBreakCount == focusSession-1){
                         shortBreakCount = 0;
                         setTimerType("Long Break");
                     }
@@ -87,9 +120,9 @@ export function Timer(props){
     
     return <div className="text-white flex flex-col bg-neutral-900/20 rounded-xl justify-between select-none p-6 lg:w-[35vw] lg:h-[42vh] md:w-[55vw] md:h-[35vw] h-[35vh] w-[95vw]">
         <div className="flex flex-row md:text-[18px] gap-6 justify-center">
-            <button onClick={()=>{setTimerType("Pomodoro"); setTimer(false); setMin(props.pomomin); setSec(props.pomosec)}} className={`${timertype == "Pomodoro" ? "bg-neutral-900/40 pl-1.5 pr-1.5 p-0.5 rounded-sm" : "border-0"}`}>Pomodoro</button>
-            <button onClick={()=>{setTimerType("Short Break"); setTimer(false); setMin(props.shortmin); setSec(props.shortsec)}} className={`${timertype == "Short Break" ? "bg-neutral-900/40 pl-1.5 pr-1.5 p-0.5 rounded-sm" : "border-0"}`}>Short Break</button>
-            <button onClick={()=>{setTimerType("Long Break"); setTimer(false); setMin(props.longmin); setSec(props.longsec)}} className={`${timertype == "Long Break" ? "bg-neutral-900/40 pl-1.5 pr-1.5 p-0.5 rounded-sm" : "border-0"}`}>Long Break</button>
+            <button onClick={()=>{setTimerType("Pomodoro"); setTimer(false); setMin(pomomin); setSec(pomosec)}} className={`${timertype == "Pomodoro" ? "bg-neutral-900/40 pl-1.5 pr-1.5 p-0.5 rounded-sm" : "border-0"}`}>Pomodoro</button>
+            <button onClick={()=>{setTimerType("Short Break"); setTimer(false); setMin(shortmin); setSec(shortsec)}} className={`${timertype == "Short Break" ? "bg-neutral-900/40 pl-1.5 pr-1.5 p-0.5 rounded-sm" : "border-0"}`}>Short Break</button>
+            <button onClick={()=>{setTimerType("Long Break"); setTimer(false); setMin(longmin); setSec(longsec)}} className={`${timertype == "Long Break" ? "bg-neutral-900/40 pl-1.5 pr-1.5 p-0.5 rounded-sm" : "border-0"}`}>Long Break</button>
         </div>
         
         <div className="md:text-9xl text-8xl font-bold text-center py-2">
@@ -116,7 +149,7 @@ export function Timer(props){
 
     function changeTimer(){
         if(timertype == "Pomodoro"){
-            if(shortBreakCount == props.focusSession-1){
+            if(shortBreakCount == focusSession-1){
                 setTimerType("Long Break");
                 shortBreakCount = 0;
             }
